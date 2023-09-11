a96743ea7372f563b7d9f28fca309e5d995a83aa
Dikshya <dikshya.maskey@innovatetech.co>
2023-09-11 17:57:29 +0545

    Runtime polymorphism

---
+++ Class1.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Net;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Techcraft
+{
+   
+    public  class Customer1 : ParentCustomer
+    {
+
+    }
+    public class Customer2 : ParentCustomer
+    {
+
+        string _billNumber, _billAmt;
+        public Customer2( string billNumber, string billAmt)
+        {
+            _billNumber = billNumber;
+            _billAmt = billAmt;
+
+        }
+
+        public override void AddCostumer()
+        {
+            Console.WriteLine($" Customer Type :{customerType}");
+            Console.WriteLine($"Name :{fullName} Address {address} MobNum {mobNumber} BillNum {_billNumber} BillAmount {_billAmt}");
+
+        }
+
+
+    }
+
+    public class ParentCustomer
+    {
+       public int customerType;
+     public   string fullName, address, mobNumber;
+
+        public virtual void AddCostumer()
+        {
+            Console.WriteLine($" Customer Type :{customerType}");
+
+            Console.WriteLine($"Name :{fullName} Address {address} MobNum {mobNumber}");
+        }
+    }
+
+ 
+}
+
---
+++ Program.cs
@@ -0,0 +1,63 @@
+﻿// See https://aka.ms/new-console-template for more information
+// Online C# Editor for free
+// Write, Edit and Run your C# code using C# Online Compiler
+
+using System;
+using System.Xml.Linq;
+using Techcraft;
+
+public class HelloWorld
+{
+    public static void Main(string[] args)
+    {
+        Console.WriteLine("Enter Customer Type  : ");
+       int customerType =int.Parse(Console.ReadLine());
+        Console.WriteLine("Enter Customer Name: ");
+        string name = Console.ReadLine();
+        Console.WriteLine("Enter Customer Address: ");
+        string address = Console.ReadLine();
+        Console.WriteLine("Enter Customer Mob: ");
+        string mob = Console.ReadLine();
+       
+
+        ParentCustomer customer = new ParentCustomer();
+        customer.customerType = customerType;
+        customer.address = address;
+        customer.fullName = name;
+        customer.mobNumber = mob;
+        if(customerType == 1)
+        {
+            customer = new Customer1();
+            customer.AddCostumer();
+
+        }
+        else if (customerType == 2)
+        {
+            Console.WriteLine("Enter Customer billAdd: ");
+            string billNumber = Console.ReadLine();
+            Console.WriteLine("Enter billAmt: ");
+            string bilAMt = Console.ReadLine();
+            if(string.IsNullOrEmpty(billNumber) || string.IsNullOrEmpty(bilAMt)){
+                if (string.IsNullOrEmpty(billNumber))
+                {
+                    Console.WriteLine("Bill Number Cannot be empty");
+                }
+                if (string.IsNullOrEmpty(bilAMt))
+                {
+                    Console.WriteLine("Bill Amount Cannot be empty");
+
+                }
+                Console.ReadKey();
+                return;
+            }
+           
+
+            customer = new Customer2(billNumber,bilAMt);
+            customer.AddCostumer();
+
+        }
+        Console.ReadKey();
+      
+
+    }
+}
